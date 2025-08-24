562c515aa824910d76c007459f3d57e095a225f4
saplachidisg <48733037+saplachidisg@users.noreply.github.com>
2025-08-24 10:38:05 +0300

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,15 +1,17 @@
 import yfinance as yf
 from transformers import pipeline
 import numpy as np
+import streamlit as st
 
-sentiment_model = pipeline("sentiment-analysis", model="yiyanghkust/finbert-tone")
-
+# Cached FinBERT sentiment (refresh every 1 hour)
+@st.cache_data(ttl=3600)
 def get_sentiment_news(ticker: str) -> float:
     try:
         headlines = yf.Ticker(ticker).news
         if not headlines:
             return 0.0
         texts = [h['title'] for h in headlines[:5]]
+        sentiment_model = pipeline("sentiment-analysis", model="yiyanghkust/finbert-tone")
         results = sentiment_model(texts)
         scores = [1 if r['label']=="positive" else -1 if r['label']=="negative" else 0 for r in results]
         return float(np.mean(scores))
